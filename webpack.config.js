226c2f3a94e07aa7480d9409e17d9af4e49f0e39
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2021-04-03 11:33:26 +0800

    set webpack globalObject

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path")
 module.exports = {
   entry: "./src/simple-js-export.js",
   output: {
+    globalObject: "this", // default self
     path: path.resolve(__dirname, "dist"),
     filename: "simple-js-export.js",
     library: {
